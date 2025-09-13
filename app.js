f0dbf97e584fe0cdb64705edbe082b8fbd24e3c9
shanic474 <s10shani@gmail.com>
2025-09-13 22:03:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello node JS")
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello World from Node.js!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
